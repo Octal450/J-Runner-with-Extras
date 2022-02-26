757c184acecc7249a98726eeeba8068e63c4a55c
Josh Davidson <joshdavidson2000@outlook.com>
2022-02-26 10:18:22 -0500

    System: Preparing for another release

--- J-Runner/Classes/variables.cs
+++ J-Runner/Classes/variables.cs
@@ -69,10 +69,10 @@ namespace JRunner
             MODEJR,
             MODEFW
         }
-        public static string version = "3.1.2 Beta";
+        public static string version = "3.1.2";
         public static string build = "3112." + DateTime.ParseExact(Properties.Resources.Build.Trim(), "MM-dd-yyyy HH:mm:ss", null).ToString("yyMMdd.HHmm");
         public const string staticversion = "3.1.2.0";
-        public static int revision = 16;
+        public static int revision = 17;
         public static bool iswriting;
         public static bool isscanningip = false;
         public static JR_MODE current_mode = JR_MODE.MODEJR;
