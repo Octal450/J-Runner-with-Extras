14ee41ca024b8ba6a450268ab77341baa3509c29
Josh Davidson <joshdavidson2000@outlook.com>
2023-06-03 18:39:01 -0400

    UI: LDV OK accept button

--- J-Runner/Forms/LDV.Designer.cs
+++ J-Runner/Forms/LDV.Designer.cs
@@ -125,6 +125,7 @@
             // 
             // xeBuildOptions
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(286, 62);
