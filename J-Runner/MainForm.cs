e8b8a8f0837f174e98fced04a4df9ade371b3c2b
Josh Davidson <joshdavidson2000@outlook.com>
2023-10-30 19:45:19 -0500

    System: Few fixes

--- J-Runner/MainForm.cs
+++ J-Runner/MainForm.cs
@@ -424,7 +424,7 @@ namespace JRunner
 
         #region LDrivesPanel
 
-        public void ldInfo_CloseLDClick()
+        public void ldInfo_CloseClick()
         {
             listInfo.Remove(ldInfo);
             pnlInfo.Controls.Remove(ldInfo);
@@ -439,7 +439,7 @@ namespace JRunner
 
         #endregion
 
-        #region xebuild Panel
+        #region XeBuild Panel
 
         public void xPanel_HackChanged()
         {
@@ -513,7 +513,7 @@ namespace JRunner
 
         }
 
-        public void xsvfChoice_CloseCRClick()
+        public void xsvfChoice_CloseClick()
         {
             listInfo.Remove(xsvfChoice);
             pnlInfo.Controls.Remove(xsvfChoice);
@@ -1836,6 +1836,15 @@ namespace JRunner
                 xPanel.clear();
                 variables.ctype = variables.ctypes[0];
                 txtIP.Text = txtIP.Text.Remove(txtIP.Text.LastIndexOf('.')) + ".";
+
+                if (listInfo.Contains(xsvfChoice))
+                {
+                    xsvfChoice_CloseClick();
+                }
+                if (listInfo.Contains(ldInfo))
+                {
+                    ldInfo_CloseClick();
+                }
             }
 
             progressBar.Value = progressBar.Minimum;
@@ -1908,7 +1917,7 @@ namespace JRunner
                 else txtCPUKey.BeginInvoke(new Action(() => txtCPUKey.Text = variables.cpukey));
 
                 Console.WriteLine("Initializing {0}, please wait...", Path.GetFileName(variables.filename1));
-                if (listInfo.Contains(xsvfChoice)) xsvfChoice_CloseCRClick();
+                if (listInfo.Contains(xsvfChoice)) xsvfChoice_CloseClick();
                 updateProgress(progressBar.Maximum / 2);
                 nand = new Nand.PrivateN(variables.filename1, variables.cpukey);
                 if (!nand.ok)
@@ -3640,7 +3649,7 @@ namespace JRunner
         {
             if (listInfo.Contains(xsvfChoice))
             {
-                xsvfChoice_CloseCRClick();
+                xsvfChoice_CloseClick();
             }
             else
             {
