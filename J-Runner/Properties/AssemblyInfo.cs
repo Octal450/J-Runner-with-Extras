54c6ce0bbc4d32b3cc0d8f02ec41a6f56ef390c8
Josh Davidson <joshdavidson2000@outlook.com>
2024-01-03 14:56:43 -0500

    System: Preparing for another release

--- J-Runner/Properties/AssemblyInfo.cs
+++ J-Runner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("J-Runner with Extras")]
 [assembly: AssemblyProduct("J-Runner with Extras")]
-[assembly: AssemblyCopyright("Copyright © J-Runner with Extras Developers 2012-2023")]
+[assembly: AssemblyCopyright("Copyright © J-Runner with Extras Developers 2012-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
